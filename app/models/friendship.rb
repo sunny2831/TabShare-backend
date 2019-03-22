f38d74c283325f20d36b79ce6dbd3d3ff1b092fe
github username <github email address>
2019-03-22 12:48:30 +0000

    modified friendship model with relationships based on tabs

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,20 +1,20 @@
 class Friendship < ApplicationRecord
 
-  def friend_history
-    final = []
+  def tab_friendships
+    relationships = []
     Tab.where(owed_by_user_id: user1_id, owed_to_user_id: user2_id).each do |tab|
-      final << tab
+      relationships << tab
       tab.payments.each do |payment|
-        final << payment
+        relationships << payment
       end
     end
     Tab.where(owed_by_user_id: user2_id, owed_to_user_id: user1_id).each do |tab|
-      final << tab
+      relationships << tab
       tab.payments.each do |payment|
-        final << payment
+        relationships << payment
       end
     end
-    (final.sort_by {|item| item.created_at}).reverse
+    (relationships.sort_by {|item| item.created_at}).reverse
   end
 
 end
